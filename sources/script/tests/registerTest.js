5fdd4b8842be07fa3f985c801d6d3c5f49bf46ce
Yann Floch <yann.floch5@libertysurf.fr>
2015-03-22 22:24:00 +0100

    add the stop recorder

--- sources/script/tests/registerTest.js
+++ sources/script/tests/registerTest.js
@@ -3,18 +3,12 @@ f = require('utils').format;
 ROOT_Path = '/home/yann/sources/conference/btd2015/sources/script';
 phantom.page.injectJs(ROOT_Path +'/casperCustomScripts/initialization.js');
 
-var searchPage = new SearchPage();
-var mainMenu = new MainMenu();
-
-
-casper.test.comment('Step 1 - Luanch the pixel tracking recorder');
+casper.test.comment('Step 1 - Launch the pixel tracking recorder');
 pixelTrackingRecorder.start();
 nbPixelTracking=pixelTrackingRecorder.getNumberOfPixelTracking();
 	
 casper.test.comment('Step 2 - Visit registration page');
 casper.start('http://btdconf.com/registration/', function() {
-
-
 
 casper.waitFor(function check() {
     return pixelTrackingRecorder.getNumberOfPixelTracking()>nbPixelTracking + 1;  
@@ -22,10 +16,10 @@ casper.waitFor(function check() {
 	function then() {
 		casper.test.assertEquals(pixelTrackingRecorder.getValueForTheKeyForTheLastRecord('events'),"scAdd","add to cart should be tracked");
 	}
-);
-
+	);
 });
-
+casper.test.comment('Step 3 - Stop the pixel tracking recorder');
+pixelTrackingRecorder.stop();
 
 casper.run(function() {
     this.test.done(1);
