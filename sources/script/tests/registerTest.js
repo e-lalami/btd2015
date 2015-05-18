dd7a82c823d944e644c52b993b6b19823d09e78c
Yann Floch <yann.floch5@libertysurf.fr>
2015-05-18 22:24:14 +0200

    remove depracated

--- sources/script/tests/registerTest.js
+++ sources/script/tests/registerTest.js
@@ -1,32 +1,38 @@
 f = require('utils').format;
+if(casper.cli.has("ROOT_Path")) 											//********************// 
+	{ ROOT_Path =casper.cli.get("ROOT_Path")}                               // Prepare the test   //
+else                                                                        //                    //
+		{ROOT_Path = '..'}                                                  //                    //
+phantom.page.injectJs(ROOT_Path +'/casperCustomScripts/initialization.js'); //********************//
 
-if(casper.cli.has("ROOT_Path"))
-	{ ROOT_Path =casper.cli.get("ROOT_Path")}
-else 
-	{ROOT_Path = '/home/yann/sources/conference/btd2015/sources/script'}
-phantom.page.injectJs(ROOT_Path +'/casperCustomScripts/initialization.js');
-
-casper.test.comment('Step 1 - Launch the pixel tracking recorder');
-pixelTrackingRecorder.start();
-nbPixelTracking=pixelTrackingRecorder.getNumberOfPixelTracking();
-	
-casper.test.comment('Step 2 - Visit registration page');
-casper.start('http://btdconf.com/registration/', function() {
-
+casper.test.comment('Step 1 - Launch the pixel tracking recorder');         //*******************//
+                                                                            // Start the pixel   //
+pixelTrackingRecorder.start();                                              // Tracking          //
+nbPixelTracking=pixelTrackingRecorder.getNumberOfPixelTracking();           // Recorder          //
+                                                                            //*******************//	
+debugger;
+casper.test.comment('Step 2 - Visit registration page');                    //*******************//
+casper.start('http://btdconf.com/registration/', function() {               // Simulate          //
+	                                                                        // Producer          //
+	                                                                        // Action            //
+                                                                            //*******************//	
 casper.waitFor(function check() {
-    return pixelTrackingRecorder.getNumberOfPixelTracking()>nbPixelTracking;  
-	}, 
-	function then() {
-		casper.test.assertEquals(pixelTrackingRecorder.getValueForTheKeyForTheLastRecord('events'),"scAdd","add to cart should be tracked");
-	}
+    return pixelTrackingRecorder.getNumberOfPixelTracking()>nbPixelTracking;//*******************//  
+	},                                                                      // test              //
+	function then() {                                                       //                   //
+		casper.test.assertEquals(                                           //                   //
+		pixelTrackingRecorder.getValueForTheKeyForTheLastRecord('events')   //                   //
+		,"scAdd"                                                            //                   //
+		,"add to cart should be tracked");                                  //                   //
+	}                                                                       //*******************//
 	);
 });
-casper.then(function() {
-	casper.test.comment('Step 6 - Stop the pixel tracking recorder');
-	pixelTrackingRecorder.stop();
-});
-
+casper.then(function() {                                                    //*******************//
+	casper.test.comment('Step 3 - Stop the pixel tracking recorder');       // Stop the pixel    //
+	pixelTrackingRecorder.stop();                                           // Tracking          //
+});                                                                         // Recorder          //
+                                                                            //*******************//	
 casper.run(function() {
-    this.test.done(1);
+    this.test.done();
 });
 
