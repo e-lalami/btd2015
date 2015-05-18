bd53daafea84630d94a968b512a27512ab5e80ed
Yann Floch <yann.floch5@libertysurf.fr>
2015-05-18 22:22:16 +0200

    remove depracated

--- sources/script/tests/analyticsTest.js
+++ sources/script/tests/analyticsTest.js
@@ -1,33 +1,27 @@
 f = require('utils').format;
+if(casper.cli.has("ROOT_Path")) 											//******************// 
+	{ ROOT_Path =casper.cli.get("ROOT_Path")}                               // Set up the test  //
+else                                                                        //                  //
+		{ROOT_Path = '..'}                                                  //                  //
+phantom.page.injectJs(ROOT_Path +'/casperCustomScripts/initialization.js'); //******************//
 
-if(casper.cli.has("ROOT_Path"))
-	{ ROOT_Path =casper.cli.get("ROOT_Path")}
-else 
-	{ROOT_Path = '/home/yann/sources/conference/btd2015/sources/script'}
-phantom.page.injectJs(ROOT_Path +'/casperCustomScripts/initialization.js');
-
-var searchPage = new SearchPage();
-var mainMenu = new MainMenu();
-
-
-casper.test.comment('Step 1 - Open Google');
-
-casper.start('http://google.fr/', function() {
-
-casper.test.comment('Step 2 - Search for btd conf');
-searchPage.fillSearchFormAndSubmit('btd conf');
-
-casper.test.comment('Step 3 - Choose the first result to btd home page');
-searchPage.chooseFirstResult();
-
-casper.test.comment('Step 4 - Visit btd home page');  
-
-casper.test.comment('Step 5 - Contact BTD team');
-casper.then(function() {
-	mainMenu.contact();
-	pixelTrackingRecorder.start();
+var searchPage = new SearchPage();                                          //**********************//
+var mainMenu = new MainMenu();                                              // Declare page objects //
+                                                                            //**********************//
+casper.test.comment('Step 1 - Open Google');                                
+casper.start('http://google.fr/', function() {                              //***************//
+                                                                            // Simulate      //
+casper.test.comment('Step 2 - Search for btd conf');                        // the producer  //
+searchPage.fillSearchFormAndSubmit('btd conf');                             // interactions  //
+                                                                            //               //
+casper.test.comment('Step 3 - Choose the first result to btd home page');   //               //
+searchPage.chooseFirstResult();                                             //               //
+                                                                            //               //
+casper.test.comment('Step 4 - Contact BTD team');                           //               //
+casper.then(function() {                                                    //               //
+	pixelTrackingRecorder.start();                                          //               //
+	mainMenu.contact();                                                     //***************//
 });
-
 
 
 var nbPixelTracking = pixelTrackingRecorder.getNumberOfPixelTracking();
@@ -36,17 +30,21 @@ casper.waitFor(function check() {
     return pixelTrackingRecorder.getNumberOfPixelTracking()>nbPixelTracking ;  
 	}, 
 	function then() {
-		casper.test.assertEquals(pixelTrackingRecorder.getValueForTheKeyForTheLastRecord('_ref'),"http://www.google.fr/search?hl=fr&source=hp&q=btd+conf&gbv=2&oq=&gs_l=","referer should be propagated");
+		casper.test.assertEquals(pixelTrackingRecorder.getValueForTheKeyForTheLastRecord('_ref'),
+		"http://www.google.fr/search?hl=fr&source=hp&q=btd+conf&gbv=2&oq=&gs_l=",
+		"referer should be propagated");
 	}
 	);
 });
 casper.then(function() {
 	casper.test.comment('Step 6 - Stop the pixel tracking recorder');
 	pixelTrackingRecorder.stop();
+
 });
 
 
 casper.run(function() {
-    this.test.done(1);
+    this.test.done();
+    
 });
 
