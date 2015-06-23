1b7e27d87dfa8e65635359d127a13e92c46d6a06
unknown <yann.floch@PAR-LAP-1388.valtech.com>
2015-06-23 21:26:46 +0200

    add more log to pixel tracking recorder

--- sources/script/waTools/pixelTrackingRecorder.js
+++ sources/script/waTools/pixelTrackingRecorder.js
@@ -1,4 +1,4 @@
-function pixelTrackingRecorder() {
+function pixelTrackingRecorder(casper) {
 	this.started=false;
 	this.analyticUrl="";
     this.analyticUrls=new Array();
@@ -18,7 +18,10 @@ function pixelTrackingRecorder() {
 	};
 	
 	this.addRecord= function(url){
+		//casper.log('pixel addRecord : ' + url , 'debug')
 		this.analyticUrls.push(url);
+		casper.log('number of url ' + this.getNumberOfPixelTracking() , 'debug');
+		this.logUrlParamValue(url);
 	};
 	
 	this.getNumberOfPixelTracking=function(){
@@ -28,7 +31,7 @@ function pixelTrackingRecorder() {
 	
 	this.getValueForTheKeyForTheLastRecord=function(name) {
 	searchLocation=this.analyticUrls[this.analyticUrls.length-1].toString().split("?")[1];
-	searchDecoded = decodeURIComponent(searchLocation)
+	searchDecoded = decodeURIComponent(searchLocation);
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(searchLocation);
@@ -37,6 +40,19 @@ function pixelTrackingRecorder() {
 	
 	this.getAllPixelTrackings=function(){
 		return this.analyticUrls.toString();
+	};
+	
+	this.logUrlParamValue=function(url){
+		var params = url.substr(url.indexOf("?")+1);
+		  
+		  params = params.split("&");
+		    // split param and value into individual pieces
+		    for (var i=0; i<params.length; i++)
+		       {
+		         temp = params[i].split("=");
+		         //if ( [temp[0]] == sname ) { sval = temp[1]; }
+		         casper.log(  temp[0] + ' : ' + decodeURIComponent(temp[1]) , 'debug');
+		       }
 	};
 
 }	
