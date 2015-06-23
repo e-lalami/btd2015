5001d7e634360bac7ca287c08647fee74a8b69f2
unknown <yann.floch@PAR-LAP-1388.valtech.com>
2015-06-23 22:04:20 +0200

    better trace for test failure

--- sources/script/casperCustomScripts/initialization.js
+++ sources/script/casperCustomScripts/initialization.js
@@ -17,15 +17,19 @@ var lastMessageRemote="";
 
 casperEvents.declareOnRessourceReceived(casper,pixelTrackingRecorder);
 
-casper.test.on('fail', function(failure) {
-    // failure type : assert or fail
-    if(failure.type.indexOf('assert') !==-1 || failure.type ==='fail'){
-        failure.description = "Message : " + failure.message + "\nLine : " + failure.line + "\nCode : " + failure.lineContents;
-    }
-    
-});
 
 casper.on('remote.message', function(msg) {
    // casper.log('remote message caught: ' + msg,'info');
     lastMessageRemote=msg;
+});
+
+casper.test.on("fail", function(failure) {
+    //if error type undefined function
+    if(failure.message.message){//or failure.message.stack.TypeError
+        failure.message.message = "Message : " + failure.message.message + "\nLine : "+ failure.message.line;//in jenkins -> title
+    }
+    //else assert error
+    else{failure.message = "Message : " + failure.message + "\nLine : "+ failure.line + "\nCode : " + failure.lineContents;}
+
+    //console.log(JSON.stringify(failure,4,'\t')); //see parameters you can modify in the failure object
 });
